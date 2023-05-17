428156410ec92d626f7c935785dce6957f13b3fc
capeloo <caiocapelo@alu.ufc.br>
2023-05-17 11:56:46 -0300

    up

---
+++ shop/lib/screens/products_overview_page.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:shop/data/dummy_data.dart';
+import 'package:shop/models/product.dart';
+import 'package:shop/widgets/product_item.dart';
+
+class ProductsOverviewPage extends StatelessWidget {
+  final List<Product> loadedProducts = dummyProducts;
+  ProductsOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black54,
+        title: const Text('Minha loja'),
+        centerTitle: true,
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(product: loadedProducts[i]),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+      ),
+    );
+  }
+}
